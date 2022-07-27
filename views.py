10caeb8246bad56f0d375b1503801c72b0858117
venubabu93 <vaddimukkala.venu93@gmail.com>
2022-07-27 21:19:52 +0530

    views.py file added

--- views.py
+++ views.py
@@ -1,2 +1,6 @@
 here is some thing about tracepharm project views.py file need to upload here
 another files need to add here
+
+this is after uploading retriving git file and now uploading to git
+
+this is git
